f09e8eb595927861fbd717fbc7d864cf4c31a00c
Ingo Albrecht <prom@berlin.ccc.de>
2013-04-14 00:39:10 +0200

    Comments

--- common/src/main/java/better/jsonrpc/client/JsonRpcClientRequest.java
+++ common/src/main/java/better/jsonrpc/client/JsonRpcClientRequest.java
@@ -15,17 +15,37 @@ import java.util.concurrent.locks.Condition;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 
+/**
+ * JSON-RPC client requests
+ *
+ * These are used to keep track of outstanding requests in the client.
+ *
+ * They therefore also contain a considerable part of call logic.
+ *
+ */
 public class JsonRpcClientRequest {
-    JsonRpcClient mClient;
-    Lock mLock;
-    Condition mCondition;
-    String mId;
-    boolean mDisconnected;
-    Throwable mException;
-    ObjectNode mRequest;
-    ObjectNode mResponse;
-    JsonRpcConnection mConnection;
 
+    /** Connection used for the request */
+    JsonRpcConnection mConnection;
+    /** Client tracking this request */
+    JsonRpcClient mClient;
+
+    /** Lock for request state */
+    Lock mLock;
+    /** Condition on mLock */
+    Condition mCondition;
+    /** Request id */
+    String mId;
+    /** Flag to indicate abort by disconnect */
+    boolean mDisconnected;
+    /** Exception that aborted this request */
+    Throwable mException;
+    /** JSON request */
+    ObjectNode mRequest;
+    /** JSON response */
+    ObjectNode mResponse;
+
+    /** Constructs a client request */
     public JsonRpcClientRequest(String id, ObjectNode request, JsonRpcConnection connection) {
         mLock = new ReentrantLock();
         mCondition = mLock.newCondition();
@@ -36,18 +56,27 @@ public class JsonRpcClientRequest {
         mClient = connection.getClient();
     }
 
+    /** Returns the request id of this request */
     public String getId() {
         return mId;
     }
 
+    /** Returns the connection used */
     public JsonRpcConnection getConnection() {
         return mConnection;
     }
 
+    /** Returns the client this request is for */
+    public JsonRpcClient getClient() {
+        return mClient;
+    }
+
+    /** Returns true if this request is done */
     private boolean isDone() {
         return mDisconnected || mResponse != null || mException != null;
     }
 
+    /** Should be called when underlying connection fails */
     public void handleDisconnect() {
         mLock.lock();
         try {
@@ -59,6 +88,7 @@ public class JsonRpcClientRequest {
         }
     }
 
+    /** Should be called on IO errors, timeouts and other such local abort causes */
     public void handleException(Throwable exception) {
         mLock.lock();
         try {
@@ -71,6 +101,7 @@ public class JsonRpcClientRequest {
         }
     }
 
+    /** Should be called when a matching response has been received */
     public void handleResponse(ObjectNode response) {
         mLock.lock();
         try {
@@ -83,16 +114,28 @@ public class JsonRpcClientRequest {
         }
     }
 
+    /**
+     * Wait for this request to finish and take appropriate action
+     *
+     * This will throw exceptions as resolved by the error resolver.
+     *
+     * If none occur, the return value of the RPC call is returned.
+     *
+     * This call will block, limited by the request timeout.
+     */
     public Object waitForResponse(Type returnType) throws Throwable {
         mLock.lock();
         try {
             // wait until done or timeout is reached
             long timeout = System.currentTimeMillis() + mClient.getRequestTimeout();
             while (!isDone()) {
+                // recompute time left
                 long timeLeft = timeout - System.currentTimeMillis();
+                // throw on timeout
                 if (timeLeft <= 0) {
                     throw new TimeoutException("JSON-RPC timeout");
                 }
+                // wait for state changes
                 try {
                     mCondition.await(timeLeft, TimeUnit.MILLISECONDS);
                 } catch (InterruptedException e) {
@@ -113,7 +156,6 @@ public class JsonRpcClientRequest {
             if (mResponse.has("error")
                     && mResponse.get("error") != null
                     && !mResponse.get("error").isNull()) {
-
                 // resolve and throw the exception
                 if (mClient.getExceptionResolver() == null) {
                     throw DefaultExceptionResolver.INSTANCE.resolveException(mResponse);
@@ -130,12 +172,13 @@ public class JsonRpcClientRequest {
                     // XXX warn
                     return null;
                 }
-
+                // get the object mapper for conversion
                 ObjectMapper mapper = mClient.getObjectMapper();
-
+                // create a parser for the result
                 JsonParser returnJsonParser = mapper.treeAsTokens(mResponse.get("result"));
+                // determine type to convert to
                 JavaType returnJavaType = TypeFactory.defaultInstance().constructType(returnType);
-
+                // parse, convert and return
                 return mapper.readValue(returnJsonParser, returnJavaType);
             }
         } finally {
