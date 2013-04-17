640647f3e71b7408ebcaf511541ba6a64f9e5290
Ingo Albrecht <prom@berlin.ccc.de>
2013-04-17 23:29:10 +0200

    Comment

--- annotations/src/main/java/better/jsonrpc/annotations/JsonRpcNotification.java
+++ annotations/src/main/java/better/jsonrpc/annotations/JsonRpcNotification.java
@@ -5,6 +5,9 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * Marks a JSON-RPC method as a notification
+ */
 @Target(ElementType.METHOD)
 @Retention(RetentionPolicy.RUNTIME)
 public @interface JsonRpcNotification {
